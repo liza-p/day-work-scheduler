0eac2fd298e7dabaa57920ba09f3ddb9a6c0d8b1
liza p <liyourlp@gmail.com>
2020-06-25 09:57:17 -0700

    add comments

--- script.js
+++ script.js
@@ -3,21 +3,28 @@
 
 let rows = document.querySelectorAll(".row");
 //console.log(rows);
+// converting values from localStr into {} & assigning to schedule var 
 let schedule = JSON.parse(localStorage.getItem('schedule')) || {};
-
+// console.log(schedule);
 
 
 for(let i = 0; i < rows.length; i++) {
+ 
     let currentRow = rows[i];
    // console.log(i, currentRow);
+
    let saveButton = currentRow.querySelector("button");
    let textArea = currentRow.querySelector("textarea")
-   let rowHour = currentRow.getAttribute("data-hour");
-   textArea.value = schedule[rowHour] || "";
-    
+   let rowTime = currentRow.getAttribute("data-hour");
+   
+   textArea.value = schedule[rowTime] || "";
+//    console.log(schedule[rowTime]);
+
+//
    saveButton.addEventListener('click', function(){
     let event = textArea.value.trim();
-    schedule[rowHour]=event;
+    schedule[rowTime]=event;
+    //saving values to localstr converting obj to ""
     localStorage.setItem('schedule',JSON.stringify(schedule) );
 
 
